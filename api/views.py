ac3d92cc7b94706f789fc6e4e422b26cd42af43a
Pavel Panchenko <mutedop@yandex.ru>
2021-05-21 08:12:01 +0800

    Review revisions 2. (1) Typed by views for permissions, filters. (2) Fixed error post request for comment - asked to explicitly indicate the post when submitting, fixed to automatically add id post. - serializers Comment. (3) Corrected read.me documentation for comment request.

--- api/views.py
+++ api/views.py
@@ -29,9 +29,9 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly]
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly, )
     filter_backends = (DjangoFilterBackend, )
-    filterset_fields = ['group']
+    filterset_fields = ('group', )
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -47,7 +47,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     we pull on the primary key.
     """
     serializer_class = CommentSerializer
-    permission_classes = [IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly]
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly, )
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_pk'))
@@ -65,7 +65,7 @@ class GroupViewSet(CreateListViewsSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class FollowViewSet(CreateListViewsSet):
@@ -73,9 +73,9 @@ class FollowViewSet(CreateListViewsSet):
     ModelViewSet written from the documentation. Only GET or POST method.
     """
     serializer_class = FollowSerializer
-    permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticated, )
     filter_backends = (SearchFilter, )
-    search_fields = ['=user__username', '=following__username']
+    search_fields = ('=user__username', '=following__username', )
 
     def perform_create(self, serializer):
         get_object_or_404(User, pk=self.request.user.pk)
